7baa9d099c61d18b572b78af835c318661f8a48b
1139283209@qq.com <1139283209@qq.com>
2018-04-12 22:07:44 +0800

    search

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,13 +1,18 @@
 <template>
-  <city-header></city-header>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
 </template>
 
 <script>
 import CityHeader from './components/header'
+import CitySearch from './components/search'
 export default {
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="请输入城市" class="search-input">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "~styles/varibles.styl"
+.search
+  background $bgColor
+  padding 0 .1rem
+  height:.72rem
+  .search-input
+    box-sizing border-box
+    width: 100%;
+    height:.62rem
+    padding 0 .1rem
+    line-height: .62rem;
+    text-align: center
+    border-radius .06rem
+    color #666
+</style>
